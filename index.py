2242786e586e6d04787e8ac8731bbd69e1c640c8
s1102517 <s1102517@gm.pu.edu.tw>
2022-11-07 14:58:41 +0800

    make it better

---
+++ index.py
@@ -0,0 +1,51 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
+from flask import Flask, render_template, request
+from datetime import datetime, timezone, timedelta
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    homepage = "<h1>資管二A 袁家偉 411025172 袁家偉的求職相關資訊</h1>"
+    homepage += "<a href=/mis>MIS</a><br>"
+    homepage += "<a href=/today>顯示日期時間</a><br>"
+    homepage += "<a href=/welcome?nick=袁家偉>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/me>袁家偉介網頁</a><br>"
+    homepage += "<br><a href=/read>讀取Firestore資料</a><br>"
+    return homepage
+
+@app.route("/read")
+def read():
+    Result = ""     
+    collection_ref = db.collection("靜宜資管")    
+    docs = collection_ref.order_by("mail", direction=firestore.Query.DESCENDING).get()    
+    for doc in docs:         
+        Result += "文件內容：{}".format(doc.to_dict()) + "<br>"    
+    return Result
+
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1>"
+
+@app.route("/today")
+def today():
+    tz = timezone(timedelta(hours=+8))
+    now = datetime.now(tz)
+    return render_template("today.html", datetime = str(now))
+
+@app.route("/welcome", methods=["GET", "POST"])
+def welcome():
+    user = request.values.get("nick")
+    return render_template("welcome.html", name=user)
+
+@app.route("/me")
+def me():
+    return render_template("aboutme.html")
+
+#if __name__ == "__main__":
+#    app.run()
